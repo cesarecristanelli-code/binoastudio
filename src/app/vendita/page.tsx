50027ab015856dbe51c06c88dd497d79269c6f72
Cesare Cristanelli <cesare.cristanelli@gmail.com>
2026-03-13 17:31:53 +0100

    Aggiunta della pagina Vendita

---
+++ src/app/vendita/page.tsx
@@ -0,0 +1,7 @@
+export default function LoadingVendita() {
+    return (
+        <div className="flex min-h-screen w-full justify-center items-center text-gray-800 tracking-wide font-semibold">
+            <p>Loading...</p>
+        </div>
+    )
+}
