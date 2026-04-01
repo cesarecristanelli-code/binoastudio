025524df5e56c5b254852d2e9d58f6e00faf8465
Cesare Cristanelli <cesare.cristanelli@gmail.com>
2026-04-01 20:09:57 +0200

    Realizzando il provider per il catalogo ed il form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,53 +21,51 @@ export default function Login() {
   };
 
   return (
-    <div className="pt-32">
-      <section className="min-w-2xl min-h-12.5 flex justify-center items-center pt-32">
-        <form
-          onSubmit={handleLogin}
-          className="flex flex-col gap-2 p-10 border-2 border-black text-black"
+    <section className="min-w-2xl min-h-12.5 flex justify-center items-center py-60">
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col gap-2 p-10 border-2 border-black text-black rounded-lg"
+      >
+        <h2 className="text-xl font-bold text-center mx-auto">
+          Area Riservata
+        </h2>
+        <label htmlFor="username" className="ps-2">
+          Username
+        </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+          className="p-3 border-2 border-black w-sm mb-3 rounded-xl"
+        />
+
+        <label htmlFor="password" className="ps-2">
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          className="p-3 border-2 border-black w-sm mb-3 rounded-xl"
+        />
+
+        <button
+          type="submit"
+          className="px-2 py-3 border-2 border-black cursor-pointer text-center mb-3 rounded-2xl hover:bg-gray-200"
         >
-          <h2 className="text-xl font-bold text-center mx-auto">
-            Area Riservata
-          </h2>
-          <label htmlFor="username" className="ps-2">
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            required
-            onChange={(e) => setUsername(e.target.value)}
-            className="p-3 border-2 border-black w-sm mb-3 rounded-lg"
-          />
+          Login
+        </button>
 
-          <label htmlFor="password" className="ps-2">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-            className="p-3 border-2 border-black w-sm mb-3 rounded-lg"
-          />
-
-          <button
-            type="submit"
-            className="px-2 py-3 border-2 border-black cursor-pointer text-center mb-3 rounded-2xl hover:bg-gray-200"
-          >
-            Login
-          </button>
-
-          {error && (
-            <p className="p-4 mx-auto text-center text-white border-2 border-red-600 bg-red-400">
-              {error}
-            </p>
-          )}
-        </form>
-      </section>
-    </div>
+        {error && (
+          <p className="p-4 mx-auto text-center text-white border-2 border-red-600 bg-red-400 rounded-md">
+            {error}
+          </p>
+        )}
+      </form>
+    </section>
   );
 }
