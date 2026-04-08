657f6fd2fbae9d21c0ec7e2252e6d8fc3c9947fb
Cesare Cristanelli <cesare.cristanelli@gmail.com>
2026-04-08 18:21:34 +0200

    n'attimo di aggiornamento a /vendita

--- src/app/(gestione-catalogo)/vendita/page.tsx
+++ src/app/(gestione-catalogo)/vendita/page.tsx
@@ -1,51 +1,59 @@
-import { prisma } from "@/lib/prisma";
-import CardImmobile from "@/components/vendita-section/CardImmbolie";
-import CardFooter from "@/components/vendita-section/CradImmobileFooter";
-import deleteImmobile from "@/actions/deleteImmobili";
+// import { prisma } from "@/lib/prisma";
+// import CardImmobile from "@/components/vendita-section/CardImmbolie";
+// import CardFooter from "@/components/vendita-section/CradImmobileFooter";
+// import deleteImmobile from "@/actions/deleteImmobili";
 
-export default async function CatalogoImmobili() {
-  const catalogoImmobili = await prisma.immobile.findMany({
-    include: {
-      immagini: true,
-    },
-    orderBy: {
-      id: "desc",
-    },
-  });
+// export default async function CatalogoImmobili() {
+//   const catalogoImmobili = await prisma.immobile.findMany({
+//     include: {
+//       immagini: true,
+//     },
+//     orderBy: {
+//       id: "desc",
+//     },
+//   });
 
-  if (catalogoImmobili.length === 0) {
-    return (
-      <div className="flex justify-center py-20">
-        <p className="text-xl">Non ci sono immobili nel catalogo.</p>
-      </div>
-    );
-  }
+//   if (catalogoImmobili.length === 0) {
+//     return (
+//       <div className="flex justify-center py-20">
+//         <p className="text-xl">Non ci sono immobili nel catalogo.</p>
+//       </div>
+//     );
+//   }
 
-  async function eliminaImmobile(id: string) {
-    const response = await deleteImmobile(id);
+//   async function eliminaImmobile(id: string) {
+//     const response = await deleteImmobile(id);
 
-    if (!response.success) alert(response.message);
-  }
+//     if (!response.success) alert(response.message);
+//   }
 
+//   return (
+//     <div className="w-full h-96 flex justify-center items-center my-52">
+//       {catalogoImmobili.map((i) => (
+//         <CardImmobile
+//           key={i.id}
+//           id={i.id}
+//           immagini={i.immagini}
+//           prezzo={i.prezzo}
+//           nomeImmobile={i.nome}
+//           indirizzo={i.indirizzo}
+//           elimina={eliminaImmobile}
+//         >
+//           <CardFooter
+//             metratura={i.metratura}
+//             numeroBagni={i.numeroBagni}
+//             numeroLocali={i.numeroLocali}
+//           />
+//         </CardImmobile>
+//       ))}
+//     </div>
+//   );
+// }
+
+export default function CatalogoImmobili() {
   return (
-    <div className="w-full h-96 flex justify-center items-center my-52">
-      {catalogoImmobili.map((i) => (
-        <CardImmobile
-          key={i.id}
-          id={i.id}
-          immagini={i.immagini}
-          prezzo={i.prezzo}
-          nomeImmobile={i.nome}
-          indirizzo={i.indirizzo}
-          elimina={eliminaImmobile}
-        >
-          <CardFooter
-            metratura={i.metratura}
-            numeroBagni={i.numeroBagni}
-            numeroLocali={i.numeroLocali}
-          />
-        </CardImmobile>
-      ))}
-    </div>
-  );
+    <div className="w-full mt-52 flex items-center justify-center">
+      <p className="text-xl">Loading...</p>
+    </div> 
+  )
 }
