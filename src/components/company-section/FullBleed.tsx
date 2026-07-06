4eb88a93b90cc87f5f84adafed318432e598c2ee
cesarecristanelli-code <cesare.cristanelli@gmail.com>
2026-07-06 16:04:22 +0200

    Piccola modifica in FullBleed.tsx

--- src/components/company-section/FullBleed.tsx
+++ src/components/company-section/FullBleed.tsx
@@ -1,19 +1,3 @@
-// import Image from "next/image";
-
-// export default function FullBleedImage() {
-//   return (
-//     <div className="relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw] w-screen h-100 overflow-hidden">
-//       <Image
-//         src="/vetro.jpg"
-//         alt="Paesaggio naturalistico"
-//         fill
-//         className="object-cover"
-//       />
-//       <div className="absolute inset-0 bg-linear-to-b from-[rgb(218,211,201)]/70 via-[#3C3833]/70 to-[#3C3833]/90" />
-//     </div>
-//   );
-// }
-
 import Image from "next/image";
 import Link from "next/link";
 
